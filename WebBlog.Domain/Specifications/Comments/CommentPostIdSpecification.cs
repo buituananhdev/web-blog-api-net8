c5d56f65ac607ac831a4268e6fb77990b289aded
TuanAnh1003 <anhaanh2003@gmail.com>
2024-07-07 19:49:32 +0700

    feat: comment service :sparkles:

---
+++ WebBlog.Domain/Specifications/Comments/CommentPostIdSpecification.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using WebBlog.Domain.Entities;
+using WebBog.Domain.Entities;
+using WebBog.Domain.Specifications;
+
+namespace WebBlog.Domain.Specifications.Comments
+{
+    public class CommentPostIdSpecification(Guid postId) : Specification<Comment>
+    {
+        public override Expression<Func<Comment, bool>> ToExpression()
+        {
+            return comment => comment.PostId == postId;
+        }
+    }
+}
